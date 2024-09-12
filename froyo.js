ca0f298b0184ecc985ab9ceef252d4a71d2764a7
beefchief <c.lang.1993@gmail.com>
2024-09-11 21:58:23 -0500

    created object for total order

--- froyo.js
+++ froyo.js
@@ -1,13 +1,15 @@
-const customerOrder = prompt(
+function froyoOrder(customerOrder) {
+    customerOrder = prompt(
     "Please enter your order, separating each flavor with a comma",
     "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
 );
+
 
 const order = customerOrder.split(",");
 
 console.log(order);
 
-{
+function totalVan(order){
     const van = "vanilla";
     let countVan = 0;
     for (flavor of order){
@@ -18,9 +20,10 @@ console.log(order);
         
     };
 console.log (`${countVan} order(s) of ${van}`);
+return countVan;
 }
 
-{
+function totalStraw(order){
     const straw = "strawberry";
     let countStraw = 0;
     for (flavor of order){
@@ -31,9 +34,10 @@ console.log (`${countVan} order(s) of ${van}`);
         
     };
 console.log (`${countStraw} order(s) of ${straw}`);
+return countStraw;
 }
 
-{
+function totalCof(order){
     const cof = "coffee";
     let countCof = 0;
     for (flavor of order){
@@ -43,5 +47,17 @@ console.log (`${countStraw} order(s) of ${straw}`);
         }
         
     };
+
 console.log (`${countCof} order(s) of ${cof}`);
+return countCof;
+}
+
+const fullOrder = {
+    vanilla : totalVan(order),
+    strawberry : totalStraw(order),
+    coffee : totalCof(order),
+}
+
+console.log(fullOrder);
+return fullOrder;
 }
